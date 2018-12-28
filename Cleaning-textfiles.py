d98ae3638b054084f0c30c5dbef6aabcfa91b76b
Andrew <26823162+andcarnivorous@users.noreply.github.com>
2018-12-28 16:23:25 +0100

    naming files done
    - DIVIDE BY GENDER BETTER (MORE NAMES TO LISTS)
    - CLEAN LAST FIRST LINES AND LAST LINES

--- Cleaning-textfiles.py
+++ Cleaning-textfiles.py
@@ -8,11 +8,13 @@ Created on Fri Dec 28 13:09:38 2018
 import re
 import codecs
 import os
-in_files = os.listdir("C:/Users/Sa/Dropbox/Education/Premaster CIS/Basic Programming/python/Bp-project-text-files")
+
+
+in_files = os.listdir()
 for _file in in_files:
     if _file.endswith(".txt"):
         
-        text=codecs.open("C:/Users/Sa/Dropbox/Education/Premaster CIS/Basic Programming/python/Bp-project-text-files/"+_file , "r",encoding="utf-8",errors="ignore")
+        text=codecs.open(_file , "r",encoding="utf-8",errors="ignore")
         text=text.read()
         if "Language: English" not in text:
             print(_file)
@@ -26,9 +28,9 @@ print(len(in_files))
 for _file in in_files:
     if _file.endswith(".txt"):
         
-        text=codecs.open("C:/Users/Sa/Dropbox/Education/Premaster CIS/Basic Programming/python/Bp-project-text-files/"+_file , "r",encoding="utf-8",errors="ignore")
+        text=codecs.open(_file , "r",encoding="utf-8",errors="ignore")
         text=text.read()
-        if "Author:" not in text:
+        if "Author:" not in text or "Title:" not in text:
             print(_file)
             in_files.remove(_file)
 print(len(in_files))
@@ -38,21 +40,46 @@ print(len(in_files))
 # =============================================================================
 for _file in in_files:
     if _file.endswith(".txt"):
-        
-        text=codecs.open("C:/Users/Sa/Dropbox/Education/Premaster CIS/Basic Programming/python/Bp-project-text-files/"+_file , "r",encoding="utf-8",errors="ignore")
+        text=codecs.open(_file , "r",encoding="utf-8",errors="ignore")
         text=text.read()
         title = re.search(r"Title:([\.\w ]+)",text).group(1)
-        #author = re.search(r"Author:([\.\w ]+)",text)
+        author = re.search(r"Author:(.+)",text).group(1)
         print(title)
-        file_name = title
+        file_name = author + title
+        file_name = file_name.lstrip()
         file_name = re.sub("[\. ,]+", "-", file_name)
         file_name = file_name + ".txt"
         print(file_name)
         try:
-            os.rename("C:/Users/Sa/Dropbox/Education/Premaster CIS/Basic Programming/python/Bp-project-text-files/"+_file ,"C:/Users/Sa/Dropbox/Education/Premaster CIS/Basic Programming/python/Bp-project-text-files/"+file_name)
+            os.rename(_file ,file_name)
         except:
             print(_file , "this is a double")
             continue
- 
+### COMMENT UNTIL HERE
+
+with open("male_names.txt", "r") as malenames:
+    malenames = malenames.readlines()
+
+malenames = [name.rstrip() for name in malenames]
+
+with open("female_names.txt", "r") as femalenames:
+    femalenames = femalenames.readlines()
+
+femalenames = [name.rstrip() for name in femalenames]
+
+print(femalenames)
+
+nono_files = ["male_names.txt","female_names.txt"]
 
 
+counter = 0
+for _file in os.listdir():
+    if _file.endswith(".txt") and _file not in nono_files:
+        index = _file.find("-")
+        if _file[:index] in femalenames:
+            print("Female! ", _file[:index])
+            counter += 1
+        elif _file[:index] in malenames:
+            counter += 1
+            print("Male!", _file[:index])
+            
