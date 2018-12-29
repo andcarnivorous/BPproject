867ccfa2845435158a9dbf31a515b43ede8c966e
gthuer <46245732+gthuer@users.noreply.github.com>
2018-12-29 16:12:19 +0100

    Graphs added

---
+++ graphs.py
@@ -0,0 +1,49 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+
+randmatrix = np.random.rand(9,100)
+x_axis = range(len(randmatrix[0,]))
+
+num_rows, num_columns = randmatrix.shape
+
+"""
+
+# bar plot
+
+for row in range(0, num_rows):
+    plt.subplot(3,3,row + 1)
+    plt.bar(x_axis, randmatrix[row,], width = 0.2, color="blue")
+    plt.title("plot number %d" % (row + 1))
+    plt.xlabel("words")
+    plt.ylabel("Frequencies")
+
+plt.show()
+
+"""
+
+def graphics (randmatrix):
+    
+    f=plt.figure()
+    sns.set_style("darkgrid", {"axes.facecolor": ".1", "grid.color": ".8"})
+    sns.set_context("poster")
+
+    for row in range(0, num_rows):
+        sns.distplot(randmatrix[row,], hist=False, rug = True)
+        plt.title("Frequency of Words")
+        plt.xlabel("Words")
+        plt.ylabel("Frequencies")
+graphics (randmatrix)
+
+
+
+
+# transform into function. argument of function is the matrix (randmatrix)
+
+# myfunction(randmatrix)
+
+# with different sizes of matrices. 
+
+# make labels/titles with matplotlib as before. 
+
