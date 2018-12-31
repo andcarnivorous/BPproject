66ca655f7929461f2a5dd6399fb9f2a005b677ec
Andrew <26823162+andcarnivorous@users.noreply.github.com>
2018-12-31 15:45:46 +0100

    updated paths

--- ftplib-worm.py
+++ ftplib-worm.py
@@ -1,3 +1,4 @@
+#import datacleaner
 import matplotlib
 import random
 from ftplib import FTP
@@ -10,18 +11,15 @@ print("connected")
 
 ftp.cwd("pub/docs/books/gutenberg/") #change working directory
 
-home = "/pub/docs/books/gutenberg/" #change working directory
+home = "/pub/docs/books/gutenberg/" 
 
 print("home now")
-
-
 
 print(ftp.nlst())
 
 # =============================================================================
 # 
 # =============================================================================
-
 def handleDownload(block, fileToWrite):
     fileToWrite.write(block)
 
@@ -41,102 +39,34 @@ def getdirs():
             list_dirs.append(_dir)
 
     return list_dirs
+
+dirs = []
+
+for x in range(0,3):
+    for y in range(0,10):
+        for z in range(0,10):
+            newdir = "/pub/docs/books/gutenberg/%s/%s/%s" % (x,y,z)
+            dirs.append(newdir)
+
+for _dir in dirs:
+    try:
+        ftp.cwd(_dir)
+        dirs2 = getdirs()
+        for _dir2 in dirs2:
+            try:
+                print(_dir2)
+                final = str(ftp.pwd()) + "/" + str(_dir2)
+                print(final)
+                out_file.writelines(final + "\n")
+            except:
+                continue
+        ftp.cwd(home)
+    except:
+        continue
+
+out_file.close()
     
-# =============================================================================
-# 
-
-def digger():
-# THIS FUNCTION GETS A LIST OF THE FIRST 2 LAYERS OF DIRECTORIES
-    counter = 0
-    list_of_dirs = []
-    second_list = []
-    #uses the getdirs function to get the first layer
-    list_of_dirs.extend(getdirs())
-
-    print("LIST: ", list_of_dirs)
-    
-    if len(list_of_dirs) > 0:
-        #first for loop gets the second layer
-        for directory in list_of_dirs:
-
-            ftp.cwd(directory) #cwd = change working directory
-
-            second_list.append(ftp.pwd())
-            for _dir in getdirs():
-
-                final = str(ftp.pwd()) + "/" + str(_dir)
-
-                second_list.append(final)
-
-
-            counter += 1
-            ftp.cwd("..")
-
-    #once the first two layers are done the worm function starts to dig
-    lista = worm(second_list)
-
-            
-    return lista
-
-# =============================================================================
-# 
-# =============================================================================
-def worm(_list):
-    #this function is supposed to dig down 2 levels
-    out_file = open("writingdirs.txt", "w")
-    _list = _list
-    idx = 0
-#    idx = len(_list)
-    final_list = []
-    lenlist = len(_list)
-
-    counter = 0
-    extra = 0
-    print("Worm Function")
-
-    while counter < 1:
-
-# This is like this because it would be nice to make it more efficient 
-# And able to crawl the whole tree.
-
-        try:
-            if len(_list) > 1:
-                counter += 1
-                print(len(_list[-idx:]))
-                for directory in _list:
-                    #go in the X directory in the list
-                    ftp.cwd(directory) #cwd = change working directory
-
-                    for _dir in getdirs():
-                        # for each directory in this new directory
-                        # get the name of the new dirs and add them to the
-                        # next ones to dig into
-                        final = str(ftp.pwd()) + "/" + str(_dir)
- 
-                        print(final) #THSI IS THE PRINTED DIR
-                        out_file.writelines(final + "\n")
-                        if final not in final_list:
-                            # if these dirs are not in the list of dirs to dig, add them
-                            # otherwise skip them
-                            final_list.append(final)
-                            _list.append(final)
-                            extra += 1
-                    # go back to the previous dir in order to start again
-                    ftp.cwd("..")
-                idx = extra
-            elif len(_list) > 100:
-                break
-            else:
-                break
-        except:
-            continue
-
-    out_file.close()
-    return final_list
-
-
-digger()
-
+print("FINEE")
 
 with open("writingdirs.txt", "r") as _dirs:
     _dirs = _dirs.readlines()
