c18380487d4b45faa8af8b78a24a04ea5ce20e6b
SiebeA <43171486+SiebeA@users.noreply.github.com>
2018-12-31 15:48:36 +0100

    Create filecleaner.py

---
+++ filecleaner.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 31 14:28:56 2018
+
+@author: Sa
+"""
+
+
+import re
+
+def file_cleaner (_file):
+    in_file = open(_file)
+    lines = in_file.read()
+    regex = re.search("\*\*\*\n" , lines )
+    beginning,end = regex.span()
+    lines = lines[end:]
+    regex = re.search("\*\*\*\s.+\n", lines)
+    
+    beginning,end = regex.span() # in between here is the wanted text 
+    lines = lines[:beginning]
+    
+    to_write = open(_file , "w")
+    to_write.write(lines)
+    to_write.close()
+    
+    
+    
+
