7fe0694bf68d31f7bcae57ffa4618a93619d2ad5
SiebeA <43171486+SiebeA@users.noreply.github.com>
2019-01-14 13:24:47 +0100

    Update graphs.py

--- graphs.py
+++ graphs.py
@@ -2,25 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
-randmatrix = np.random.rand(9,100)
-x_axis = range(len(randmatrix[0,]))
-
-num_rows, num_columns = randmatrix.shape
-
-"""
-
-# bar plot
-
-for row in range(0, num_rows):
-    plt.subplot(3,3,row + 1)
-    plt.bar(x_axis, randmatrix[row,], width = 0.2, color="blue")
-    plt.title("plot number %d" % (row + 1))
-    plt.xlabel("words")
-    plt.ylabel("Frequencies")
-
-plt.show()
-
-"""
 
 def graphics (randmatrix):
 
